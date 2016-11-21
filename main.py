79eb81174db4350bbbf1e7ff56140f22aee47bf4
ronaldtheodoro <ronaldtheodoro@gmail.com>
2016-11-21 00:17:58 -0200

    bot finished

--- main.py
+++ main.py
@@ -6,11 +6,8 @@ from decouple import config
 # User chrome driver
 browser = webdriver.Chrome(executable_path='drivers/chromedriver')
 
-# Linkedin's url
-url = 'https://www.linkedin.com/uas/login?goback=&trk=hb_signin'
-
 # Get html
-browser.get(url)
+browser.get('https://www.linkedin.com/uas/login?goback=&trk=hb_signin')
 
 
 def login_linkedin():
@@ -35,7 +32,11 @@ def search_linkedin(search):
     # Find search button
     browser.find_element_by_xpath(
         '//*[@id="global-search"]/fieldset/button').click()
-    # Send data and click
+
+
+def find_element(element):
+    """Find just one element"""
+    return browser.find_element_by_xpath(element)
 
 
 def close_browser():
@@ -47,15 +48,26 @@ if __name__ == '__main__':
     login_linkedin()
     search_linkedin('python')
 
-    browser.find_element_by_xpath(
-        '//*[@id="search-types"]/div/ul/li[2]/a').click()
-    browser.find_element_by_xpath(
+    find_element('//*[@id="search-types"]/div/ul/li[2]/a').click()
+
+    find_element(
         '//*[@id="facet-N"]/fieldset/div/ol/li[3]/div/label/bdi').click()
 
-    sleep(3)
+    sleep(2)
 
-    for a in browser.find_elements_by_class_name('primary-action-button'):
-        a.click()
-        input('avancar')
+    page_num = find_element('//*[@id="results_count"]/div/p/strong[1]')
+    page_num = page_num.get_attribute('textContent').replace(',', '')
+    page_num = int(page_num) // 10
+
+    url = browser.current_url
+
+    for page in range(1, page_num + 1):
+        new_url = url + '&page_num={0}'.format(page)
+        browser.get(new_url)
+
+        print('Page number: {}'.format(page))
+
+        for a in browser.find_elements_by_class_name('primary-action-button'):
+            print(a)
 
     close_browser()
